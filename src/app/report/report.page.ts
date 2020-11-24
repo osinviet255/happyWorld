d007a35ec8ef97414cf32702259f78e464377d68
hungnv1011 <hungnv1011@gmail.com>
2020-11-24 09:44:37 +0700

    23/11/2020

--- src/app/report/report.page.ts
+++ src/app/report/report.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Chart } from '../../../node_modules/chart.js';
 
 @Component({
   selector: 'app-report',
@@ -7,7 +8,69 @@ import { Component } from '@angular/core';
 })
 export class ReportPage {
 
+  private lineChart: Chart;
+  @ViewChild('lineCanvas') lineCanvas: ElementRef;
+  @ViewChild('lineCanvasLoinhuan') lineCanvasLoinhuan: ElementRef;
   constructor() { }
 
+  ionViewDidEnter() {
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: "line",
+      data: {
+        labels: ["2016", "2017", "2018", "2019"],
+        textAlign: 'right',
+        datasets: [
+          {
+            label: "Doanh thu (ĐV: Triệu đồng)",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 79, 116)',
+            borderWidth: 1,
+            pointBorderColor: false,
+            data: [5, 30, 50, 100],
+            fill: false,
+            lineTension: .4,
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              stepSize: 40
+            }
+          }]
+        }
+      }
+    });
+
+    this.lineChart = new Chart(this.lineCanvasLoinhuan.nativeElement, {
+      type: "line",
+      data: {
+        labels: ["2016", "2017", "2018", "2019"],
+        textAlign: 'right',
+        datasets: [
+          {
+            label: "Lợi nhuận (ĐV: Triệu đồng)",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 79, 116)',
+            borderWidth: 1,
+            pointBorderColor: false,
+            data: [3, 15, 40, 90],
+            fill: false,
+            lineTension: .4,
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              stepSize: 40
+            }
+          }]
+        }
+      }
+    });
+  }
 
 }
